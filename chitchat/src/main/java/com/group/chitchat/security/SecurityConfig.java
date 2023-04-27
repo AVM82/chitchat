71937b081b97be6c17cc3f3c7a0a16f76b1b4140
Roman Vainer <7019267@gmail.com>
2023-04-27 09:27:29 +0200

    added preAuthorize to CategoryController

--- chitchat/src/main/java/com/group/chitchat/security/SecurityConfig.java
+++ chitchat/src/main/java/com/group/chitchat/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -15,6 +16,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableMethodSecurity()
 public class SecurityConfig {
 
   private final JwtAuthenticationFilter jwtAuthFilter;
@@ -32,8 +34,7 @@ public class SecurityConfig {
     http
         .csrf().disable()
         .authorizeHttpRequests()
-        .requestMatchers("/api/v1/auth/**", "/*", "/v3/api-docs/**",
-            "/swagger-ui/**", "/api/v1/category/all**","api/v1/chitchats/all**")
+        .requestMatchers("/api/v1/auth/**", "/**", "/v3/api-docs/**", "/swagger-ui/**")
         .permitAll()
         .anyRequest()
         .authenticated()
