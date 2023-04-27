7ac00efa38ca13cf7b9351af1087403aaf45b9e4
Roman Vainer <7019267@gmail.com>
2023-04-27 10:44:57 +0200

    added method AddLanguage()

--- chitchat/src/main/java/com/group/chitchat/controller/LanguageController.java
+++ chitchat/src/main/java/com/group/chitchat/controller/LanguageController.java
@@ -5,14 +5,17 @@ import com.group.chitchat.service.language.LanguageService;
 import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/get_languages")
+@RequestMapping("/api/v1/languages")
 public class LanguageController {
 
   private final LanguageService languageService;
@@ -20,5 +23,11 @@ public class LanguageController {
   @GetMapping
   public ResponseEntity<Set<LanguageDto>> getAllLanguages() {
     return ResponseEntity.ok(languageService.getAvailableLanguages());
+  }
+
+  @PreAuthorize("hasRole('ROLE_ADMIN')")
+  @PostMapping
+  public ResponseEntity<LanguageDto> addLanguage(@RequestBody LanguageDto languageDto) {
+    return languageService.addLanguage(languageDto);
   }
 }
--- chitchat/src/main/java/com/group/chitchat/service/language/LanguageService.java
+++ chitchat/src/main/java/com/group/chitchat/service/language/LanguageService.java
@@ -1,10 +1,15 @@
 package com.group.chitchat.service.language;
 
+import com.group.chitchat.model.Language;
 import com.group.chitchat.model.dto.LanguageDto;
 import com.group.chitchat.repository.LanguageRepo;
+import java.util.Arrays;
+import java.util.Locale;
 import java.util.Set;
 import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 /**
@@ -25,5 +30,29 @@ public class LanguageService {
     return languageRepository.findAll()
         .stream().map(LanguageDtoService::getFromEntity)
         .collect(Collectors.toSet());
+  }
+
+  /**
+   * Adds new language to database.
+   *
+   * @param languageDto Incoming dto with new language data  for adding to database.
+   * @return Response with status and body with new language.
+   */
+  public ResponseEntity<LanguageDto> addLanguage(LanguageDto languageDto) {
+    String codeIso = languageDto.getCodeIso();
+    if (!isLanguageCorrect(codeIso)) {
+      throw new IllegalArgumentException(
+          String.format("Code ISO %s of language is not correct", codeIso));
+    } else {
+      Language language = LanguageDtoService.getFromDto(languageDto);
+      languageRepository.save(language);
+      return ResponseEntity.status(HttpStatus.CREATED)
+          .body(LanguageDtoService.getFromEntity(language));
+    }
+  }
+
+  private boolean isLanguageCorrect(String codeIso) {
+
+    return Arrays.asList(Locale.getISOLanguages()).contains(codeIso);
   }
 }
