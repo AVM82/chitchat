f4afb4eb23a6ea9d6be69c0121d653cba8b89fc9
mykola_kor <mikolakoreckij45@gmail.com>
2023-04-27 17:59:03 +0300

    chit-9: add i18n (backend) in CategoryController

--- chitchat/src/main/java/com/group/chitchat/controller/LanguageController.java
+++ chitchat/src/main/java/com/group/chitchat/controller/LanguageController.java
@@ -1,17 +1,17 @@
 package com.group.chitchat.controller;
 
 import com.group.chitchat.model.dto.LanguageDto;
-import com.group.chitchat.service.LanguageService;
 import com.group.chitchat.service.ResourcesBundleService;
+import com.group.chitchat.service.language.LanguageService;
 import java.util.Locale;
 import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
