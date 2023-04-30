1d26ac6a9ea4384fa12c655e20e662766551c8d3
Serhii Beseda <sergaandd@gmail.com>
2023-04-30 22:49:41 +0300

    add assets folder

--- chitchat/src/main/java/com/group/chitchat/security/SecurityConfig.java
+++ chitchat/src/main/java/com/group/chitchat/security/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
         .authorizeHttpRequests()
         .requestMatchers("/api/v1/auth/**", "/*", "/v3/api-docs/**",
             "/swagger-ui/**", "/api/v1/category/all**", "api/v1/chitchats/all**",
-            "api/v1/languages/**")
+            "api/v1/languages/**","assets/**")
         .permitAll()
         .anyRequest()
         .authenticated()
