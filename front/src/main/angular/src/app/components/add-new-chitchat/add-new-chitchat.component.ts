6ed2b42662d648c4312630b1eba83b4d606710fa
Serhii Beseda <sergaandd@gmail.com>
2023-04-30 15:49:36 +0300

    add new chitchat component

--- front/src/main/angular/src/app/components/add-new-chitchat/add-new-chitchat.component.ts
+++ front/src/main/angular/src/app/components/add-new-chitchat/add-new-chitchat.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import {MatDialog} from "@angular/material/dialog";
+import {Language} from "../../model/Language";
+import {Level} from "../../model/Level";
+import {Category} from "../../model/Category";
 
 @Component({
   selector: 'app-add-new-chitchat',
@@ -7,6 +10,10 @@ import {MatDialog} from "@angular/material/dialog";
   styleUrls: ['./add-new-chitchat.component.scss']
 })
 export class AddNewChitchatComponent {
+  languages: Language[];
+  levels: Level[];
+  categories: Category[];
+
   constructor(
               private dialog: MatDialog) {
   }
