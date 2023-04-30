2f6489166254032b461d7e2efc1137370603667f
Serhii Beseda <sergaandd@gmail.com>
2023-04-30 22:07:41 +0300

    CORS disabled

--- chitchat/src/main/java/com/group/chitchat/service/chitchat/ChitchatService.java
+++ chitchat/src/main/java/com/group/chitchat/service/chitchat/ChitchatService.java
@@ -57,10 +57,11 @@ public class ChitchatService {
     if (chitchatOptional.isEmpty()) {
       throw new NoSuchElementException(String.format("Chitchat with id %s not found", chitchatId));
     } else {
-      HttpHeaders header = new HttpHeaders();
-      header.set("Access-Control-Allow-Origin", "*");
-      return ResponseEntity.status(HttpStatus.OK).headers(header).body(
-          ChitchatDtoService.getFromEntity(chitchatOptional.get()));
+//      HttpHeaders header = new HttpHeaders();
+//      header.set("Access-Control-Allow-Origin", "*");
+      return ResponseEntity.
+//          status(HttpStatus.OK).headers(header).body
+              ok(ChitchatDtoService.getFromEntity(chitchatOptional.get()));
     }
   }
 
@@ -143,10 +144,11 @@ public class ChitchatService {
       chitchats = chitchats.stream().filter(chitchat -> chitchat.getLevel().name().equals(level))
           .toList();
     }
-    HttpHeaders header = new HttpHeaders();
-    header.set("Access-Control-Allow-Origin", "*");
-    return ResponseEntity.status(HttpStatus.OK).headers(header)
-        .body(chitchatFiltration(chitchats, dateFrom, dateTo));
+//    HttpHeaders header = new HttpHeaders();
+//    header.set("Access-Control-Allow-Origin", "*");
+    return ResponseEntity.
+//        status(HttpStatus.OK).headers(header).body
+            ok(chitchatFiltration(chitchats, dateFrom, dateTo));
 
   }
 
