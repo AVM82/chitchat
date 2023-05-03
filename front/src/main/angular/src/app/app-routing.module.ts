679712ed217e9dae0f8070ca9611ee3b562aa2d6
Oleksandr Hrabovets <alex6754035@gmail.com>
2023-05-03 12:59:48 +0300

    remove routing, move reference to app-component

--- front/src/main/angular/src/app/app-routing.module.ts
+++ front/src/main/angular/src/app/app-routing.module.ts
@@ -1,19 +1,11 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {NotFoundComponent} from "./components/not-found/not-found.component";
-import {BrowserModule} from "@angular/platform-browser";
-import {ChitchatRefComponent} from "./components/chitchat-ref/chitchat-ref.component";
-import {MainComponent} from "./components/main/main.component";
 
 const routes: Routes = [
-  {path: 'chitchat', component: ChitchatRefComponent},
-  {path: '', component: MainComponent},
-  {path: '**', component: NotFoundComponent}
 ];
 
 @NgModule({
-  imports: [BrowserModule, RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule { }
--- front/src/main/angular/src/app/app.component.ts
+++ front/src/main/angular/src/app/app.component.ts
@@ -1,4 +1,9 @@
 import {Component} from '@angular/core';
+import {Chitchat} from "./model/Chitchat";
+import {Subscription} from "rxjs";
+import {ChitchatService} from "./service/chitchat.service";
+import {TokenStorageService} from "./service/token-storage.service";
+import {ActivatedRoute} from "@angular/router";
 
 
 @Component({
@@ -7,5 +12,22 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  chitchatId: any = null;
+  oneChitChat: Chitchat;
+  private querySubscription: Subscription;
+
+  constructor(
+      private chitchatService: ChitchatService,
+      private tokenStorageService: TokenStorageService,
+      private route: ActivatedRoute
+  ) {
+    this.querySubscription = route.queryParams.subscribe(
+        (queryParam: any) => {
+          this.chitchatId = queryParam['id'];
+          console.log(this.chitchatId);
+          this.chitchatService.get(this.chitchatId).subscribe(value => this.oneChitChat = value);
+        }
+    );
+  }
 
 }
