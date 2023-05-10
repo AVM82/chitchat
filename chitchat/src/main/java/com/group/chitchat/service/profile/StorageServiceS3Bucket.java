73f11e3bc3ca7920a10f0f7e719373d19681972f
Roman Vainer <7019267@gmail.com>
2023-05-10 13:18:25 +0200

    add s3bucket service

--- chitchat/src/main/java/com/group/chitchat/service/profile/StorageServiceS3Bucket.java
+++ chitchat/src/main/java/com/group/chitchat/service/profile/StorageServiceS3Bucket.java
@@ -1,13 +1,45 @@
 package com.group.chitchat.service.profile;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import java.io.File;
+import java.util.Objects;
+import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
+@AllArgsConstructor
 public class StorageServiceS3Bucket implements FileStorageService {
+
+  private final Environment environment;
 
   @Override
   public String saveFile(String userName, MultipartFile file) {
-    return null;
+
+    AWSCredentials credentials = new BasicAWSCredentials(
+        Objects.requireNonNull(environment.getProperty("aws.access.key")),
+        Objects.requireNonNull(environment.getProperty("aws.secret.key"))
+    );
+
+    AmazonS3 s3client = AmazonS3ClientBuilder
+        .standard()
+        .withCredentials(new AWSStaticCredentialsProvider(credentials))
+        .withRegion(Regions.EU_CENTRAL_1)
+        .build();
+
+    s3client.putObject(
+        environment.getProperty("bucket.name"),
+        "keyDefaultAvatar",
+        new File("database/default.png"));
+
+    return s3client.getUrl(
+        environment.getProperty("bucket.name"),
+        "keyDefaultAvatar").toString();
   }
 }
