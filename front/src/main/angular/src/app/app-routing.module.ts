47e81c9af53cf33d0a4af4a6f358d8027008c37e
Oleksandr Hrabovets <alex6754035@gmail.com>
2023-05-11 11:02:05 +0300

    add routing

--- front/src/main/angular/src/app/app-routing.module.ts
+++ front/src/main/angular/src/app/app-routing.module.ts
@@ -1,11 +1,24 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {MainComponent} from "./components/main/main.component";
+import {ChitchatRefComponent} from "./components/chitchat-ref/chitchat-ref.component";
+import {NotFoundComponent} from "./components/not-found/not-found.component";
+import {BrowserModule} from "@angular/platform-browser";
+import {LoginComponent} from "./auth/login/login.component";
+import {ProfileComponent} from "./components/profile/profile.component";
+import {EmailConfirmComponent} from "./components/email-confirm/email-confirm.component";
 
 const routes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'chitchat', component: ChitchatRefComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: 'click', component: EmailConfirmComponent},
+  {path: '**', component: NotFoundComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
