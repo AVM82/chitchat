762392736b085c7d476423e274308d93875d3fa3
Serhii Beseda <sergaandd@gmail.com>
2023-05-22 13:24:38 +0300

    email confirm+one chitchat

--- front/src/main/angular/src/app/components/email-confirm/email-confirm.component.ts
+++ front/src/main/angular/src/app/components/email-confirm/email-confirm.component.ts
@@ -1,7 +1,9 @@
 import {Component} from '@angular/core';
 import {Subscription} from "rxjs";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {AuthService} from "../../service/auth.service";
+import {TokenStorageService} from "../../service/token-storage.service";
+import {NotificationService} from "../../service/notification.service";
 
 @Component({
   selector: 'app-email-confirm',
@@ -14,12 +16,14 @@ export class EmailConfirmComponent {
 
   constructor(
       private authService: AuthService,
-      private route: ActivatedRoute
+      private router: Router,
+      private route: ActivatedRoute,
+      private tokenStorage: TokenStorageService,
+      private notificationService:NotificationService
   ) {
     this.querySubscription = route.queryParams.subscribe(
         (queryParam: any) => {
           this.clickToken = queryParam['click'];
-          console.log(this.clickToken);
         }
     );
   }
@@ -28,7 +32,13 @@ export class EmailConfirmComponent {
     this.authService.newUserEmailConfirm(this.clickToken)
     .subscribe(value => {
       this.clickToken = value
-    console.log(value)
+      this.tokenStorage.saveToken(value.token);
+      // this.tokenStorage.saveRefreshToken(data.refreshToken);
+      this.tokenStorage.saveUser();
+      let currentUserId = this.tokenStorage.getUserId();
+      this.router.navigate(['/profile']);
     });
+
+    console.log("***** "+this.clickToken)
   }
 }
