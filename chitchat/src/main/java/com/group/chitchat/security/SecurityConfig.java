bc1f9b1ddd6dbd7751429739df763e628eb28bc3
roman-vainer <7019267@gmail.com>
2023-08-23 15:34:39 +0200

    cors SecurityConfig

--- chitchat/src/main/java/com/group/chitchat/security/SecurityConfig.java
+++ chitchat/src/main/java/com/group/chitchat/security/SecurityConfig.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -34,7 +33,7 @@ public class SecurityConfig {
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
         .csrf().disable()
-        .cors(Customizer.withDefaults())
+//        .cors(Customizer.withDefaults())
         .authorizeHttpRequests()
         .requestMatchers("/api/v1/auth/**", "/*", "/v3/api-docs/**",
             "/swagger-ui/**", "/api/v1/category/all/**", "api/v1/chitchats/all/**",
